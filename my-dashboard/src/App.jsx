a0daf38ecbd3642a396f49fec8e3e919c6a6feb2
H A S A N <hasan25042019@gmail.com>
2025-03-21 22:54:09 +0600

    src

--- my-dashboard/src/App.jsx
+++ my-dashboard/src/App.jsx
@@ -26,7 +26,6 @@ export default function App() {
             onClick={() => setSidebarOpen(false)}
           ></div>
         )}
-
         {/* Main Content */}
         <div className="flex-1 overflow-y-auto h-screen">
           <Header toggleSidebar={() => setSidebarOpen(!sidebarOpen)} />
--- my-dashboard/src/pages/Dashboard.jsx
+++ my-dashboard/src/pages/Dashboard.jsx
@@ -113,7 +113,6 @@ export default function Dashboard() {
               <div className="absolute bg-white/40 w-8 h-8 rounded-full translate-x-4"></div>
            </div>
 
-
           {/* Foreground Content */}
           <div className="p-6">
             <div className="flex justify-between items-center">
